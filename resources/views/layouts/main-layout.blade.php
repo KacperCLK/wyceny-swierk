a8c2b5caeb2c469c80301e4ed3d16b5cfb9a5ca8
KacperCLK <kacper.celak@gmail.com>
2025-03-04 00:22:54 +0100

    modified:   resources/views/layouts/main-layout.blade.php

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', 'Wyceny - Paweł Świerk')</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" type="image/png" href="{{ asset('storage/images/'.$media['logo']['path']) }}">
+    <title>@yield('title', 'Wyceny - Paweł Świerk | Kraków')</title>
 
-        <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="{{ asset('storage/images/'.$media['logo']['path']) }}">
 
-        <meta name="title" content="Profesjonalne Wyceny Budowlane – Szybko i Dokładnie | Paweł Świerk">
-        <meta name="description" content="Oferuję rzetelne wyceny budowlane dla klientów indywidualnych i firm. Sprawdź koszty budowy, remontu lub inwestycji – skontaktuj się już dziś!">
-        <meta name="keywords" content="wyceny budowlane, kosztorysy budowlane, wycena remontu, kosztorys inwestycji, szacowanie kosztów budowy, wycena prac budowlanych">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Forum&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        
-        <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
-        <script src="https://kit.fontawesome.com/530790a84d.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="main-layout">
-        <div class="home">
-            @yield('content')
-        </div>
-    </body>
+    <meta name="title" content="Profesjonalne Wyceny Budowlane – Szybko i Dokładnie | Paweł Świerk">
+    <meta name="description"
+        content="Kosztorysy budowlane 🛠️
+            Kraków | Małopolska: wstępne, inwestorskie, ofertowe, przedmiary, obmiary i weryfikacja kosztorysów 💼">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Forum&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+    <script src="https://kit.fontawesome.com/530790a84d.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="main-layout">
+    <div class="home">
+        @yield('content')
+    </div>
+</body>
+
 </html>
